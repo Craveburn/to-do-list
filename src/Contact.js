c269240b8e321caf83a485859c50bbf15c283ed5
Cameron Rabunn <raburncameron@gmail.com>
2018-05-21 18:10:28 -0600

    added a for style to this page via semantic

--- src/Contact.js
+++ src/Contact.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import './Contact.css'
+import { Link } from 'react-router-dom'
+import { Icon, Button, Checkbox, Form } from 'semantic-ui-react'
 
 export default class Contact extends Component {
-    render (){
-        return(
-            <div className="container2"> This is where the contact me stuff will go
-                <form>
-                   <h2>Name of List: <input/></h2>
-                   <h2>What's Your Name:<input/></h2>
-                   <h3><input type="radio" value="option1"/>Due By:<input/></h3>
-                   <h3><input type="radio" value="option2"/>No Due Date</h3>
-                </form>
+    render() {
+        return (
+            <div className="container2"> Send It Boi
+                <Form>
+                    <Form.Field>
+                        <label>Name</label>
+                        <input placeholder='Put Your Name Here' />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Last Name</label>
+                        <input placeholder='Last Name' />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox label='I agree to the Terms and Conditions' />
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+                <Link to="/">  <Icon color="black" name="home" size="big" /> </Link>
             </div>
         )
     }
