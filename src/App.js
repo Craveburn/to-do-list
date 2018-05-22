57e8aab1da4f0ec13e213bbc26fdc124ef6ed4c6
Cameron Rabunn <raburncameron@gmail.com>
2018-05-21 18:09:44 -0600

    imported browser-router and created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import Home from './Home'
+import Contact from './Contact'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={() => <Home />} />
+            <Route path="/Contact" component={() => <Contact />} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
