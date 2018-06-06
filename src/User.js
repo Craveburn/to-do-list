626e80072426c05a7468d3c5cf781182b041bb03
Cameron Rabunn <raburncameron@gmail.com>
2018-06-06 00:35:06 -0600

    updated some semantic UI shit.

--- src/User.js
+++ src/User.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import ReactPropTypes from 'prop-types'
 import './User.css'
+import { Icon, Button } from 'semantic-ui-react'
 
-export default class User extends Component{
+export default class User extends Component {
     static propTypes = {
         createToDo: ReactPropTypes.string,
     }
     render() {
-        return(
+        return (
             <div className="listItem">
                 {this.props.createToDo}
+                <Button  circular icon>
+                <Icon size='small' circular color='white' name='eraser' />
+                </Button>
             </div>
         )
     }
