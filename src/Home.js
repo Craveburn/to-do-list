625d806b41566f2658de175bd71f218c376a2bce
Cameron Rabunn <raburncameron@gmail.com>
2018-06-06 00:32:58 -0600

    added an async await promise.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactPropTypes from 'prop-types'
-import User from './User'
 import './Home.css'
+import ListItems from './ListItems'
 import { Link } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
 
@@ -9,9 +9,7 @@ import { Icon } from 'semantic-ui-react'
 export default class Home extends Component {
 
     state = {
-        users: [
 
-        ]
     }
 
     getInfo = async () => {
@@ -25,60 +23,99 @@ export default class Home extends Component {
     }
 
 
-handleOnChange = (e) => {
-    if (e.target.id === "toDoEntry")
-        this.setState({
-            createToDo: e.target.value
-        })
-}
+    handleOnChange = (e) => {
+        if (e.target.id === "toDoEntry")
+            this.setState({
+                createToDo: e.target.value
+            })
+    }
 
-handleOnClick = () => {
-    this.setState((prev) => {
-        const existingUsers = prev.users
-        existingUsers.push({
-            createToDo: prev.createToDo
-        })
-        return {
-            users: existingUsers
+    handleOnClick = async () => {
+        // this.setState((prev) => {
+        //     const existingUsers = prev.users
+        //     existingUsers.push({
+        //         createToDo: prev.createToDo
+        //     })
+        // })
+        const listData = {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
         }
-    })
-}
+        try {
+            const postList = await fetch('http://localhost:3001/list', listData)
+            const list = await postList.json()
+            this.renderList()
+            this.setState({
 
-renderList = () => {
-    return this.state.users.map((user, i) => {
+            })
+            console.log("this is the list", list)
+        } catch (error) {
+            console.log("error", error)
+        }
+        // return {
+        //     users: existingUsers
+        // }
+    }
+
+    mapList = () => {
+        if(!this.state.users){
+            return <div> ...Loading</div>
+        }else{
+            const theUsers = this.state.users
+            console.log("mapList", theUsers.users)
+            return theUsers.map((key) => {
+            return <li key={key.id} >{key.createToDo}</li>
+       })
+        }
+    }
+
+
+    renderList = async () => {
+        const getList = await fetch("http://localhost:3001/list")
+        const listItems = await getList.json()
+        console.log("listItems", listItems)
+        this.setState({
+            users: listItems
+        })
+        // return listItems.map((user, i) => {
+        //     return (
+        //         <li key={i}><User createToDo={user.createToDo} /></li>
+        //     )
+        // })
+    }
+
+    componentDidMount() {
+        this.renderList()
+    }
+
+    render() {
+        console.log(this.state.users)
+
         return (
-            <li key={i}><User createToDo={user.createToDo} /></li>
-        )
-    })
-}
+            <div className="container">
+                <header>
+                    <h1>What Do You Need To Do?</h1>
+                    <div><input id="toDoEntry" onChange={this.handleOnChange} />
+                        <button onClick={this.handleOnClick}>Add</button>
+                    </div>
+                </header>
+                <article className="listBody">
+                    <h1 className="titleName">What Ya Gotta Do</h1>
+                    <div className="renderedList">
+                        <ul>
+                            <ListItems data={this.state.users}/>
+                        </ul>
+                    </div>
 
-componentDidMount() {
-    this.getInfo()
-}
-
-render() {
-    console.log(this.state)
-    return (
-        <div className="container">
-            <header>
-                <h1>What Do You Need To Do?</h1>
-                <div><input id="toDoEntry" onChange={this.handleOnChange} />
-                    <button onClick={this.handleOnClick}>Add</button>
+                </article>
+                <div className="icon">
+                    <Link to="/Contact">  <Icon color="black" name="mail" size="big" /> </Link>
                 </div>
-            </header>
-            <article className="listBody">
-                <h1 className="titleName">What Ya Gotta Do</h1>
-                <div className="renderedList">
-                    <ul>
-                        {this.renderList()}
-                    </ul>
-                </div>
-
-            </article>
-            <div className="icon">
-                <Link to="/Contact">  <Icon color="black" name="mail" size="big" /> </Link>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
