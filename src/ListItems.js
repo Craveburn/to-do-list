515cb95801c8c89e6c852351656950a636dfb603
Cameron Rabunn <raburncameron@gmail.com>
2018-06-06 18:17:06 -0600

    added some buttons/bulleted list for the to do list

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,19 +1,26 @@
-import React, {Component} from 'react'
-import { Divider } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Icon, Button } from 'semantic-ui-react'
+import './ListItems.css'
 
 export default class extends Component {
-    render(){
-console.log(this.props.data)
-        if(this.props.data == undefined){
-            return(
-                <div>...loading</div>
+    render() {
+        console.log(this.props.data)
+        if (this.props.data == undefined) {
+            return (
+                <div><Icon loading name='spinner' /></div>
             )
         }
-        else{
-
-            return this.props.data.map((key)=> {
+        else {
+            return this.props.data.map((key) => {
                 console.log(key)
-                return <div key={key.id}>{key.createToDo}</div>
+                return <div className='listItem' key={key.id}>
+                    <ul>
+                        <li>{key.createToDo}
+                        <Button className='button' size='mini' circular color='red' icon='delete'/>
+                        <Button circular icon='edit'/>
+                        </li>
+                    </ul>
+                </div>
             })
         }
     }
