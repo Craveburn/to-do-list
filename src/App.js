075b9472b8eeaa241b2adfecb6903b0f8769b6f4
Cameron Rabunn <raburncameron@gmail.com>
2018-06-07 19:50:06 -0600

    Took out the create-react-app css styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import Home from './Home'
-import Contact from './Contact'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css'
 
 class App extends Component {
   render() {
@@ -11,7 +9,6 @@ class App extends Component {
         <div className="App">
           <Switch>
             <Route exact path="/" component={() => <Home />} />
-            <Route path="/Contact" component={() => <Contact />} />
           </Switch>
         </div>
       </Router>
