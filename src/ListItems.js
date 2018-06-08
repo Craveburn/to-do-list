c872d1915fda3ba5498d6183efc23251cfe3272b
Cameron Rabunn <raburncameron@gmail.com>
2018-06-07 19:52:18 -0600

    fixed the issue where the console was mad I wasn't specifying id and added my modal component

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
+import EditModal from './EditModal'
 import { Icon, Button } from 'semantic-ui-react'
 import './ListItems.css'
 
 export default class extends Component {
 
     deleteFunction = (id) => {
-        console.log('delete button hit', id)
        this.props.deleteFunction(id)
     }
 
     render() {
-        console.log(this.props.data)
         if (this.props.data === undefined) {
             return (
                 <div><Icon loading name='spinner' /></div>
@@ -18,15 +17,13 @@ export default class extends Component {
         }
         else {
             return this.props.data.map((key) => {
-                console.log(key._id)
-                return <div className='listItem' key={key.id}>
+                return <div className='listItem' key={key._id}>
                     <ul>
                         <li>{key.createToDo}
                         <Button className='button' size='mini' circular color='red' icon='delete' onClick={() => {
                             this.deleteFunction(key._id)
                         }}/>
-
-                            <Button circular icon='edit' />
+                            <EditModal data={key} />
                         </li>
                     </ul>
                 </div>
