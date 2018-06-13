5c8c7f12156a20602a2e8a3c9d667e112be537c3
Cameron Rabunn <raburncameron@gmail.com>
2018-06-12 19:53:14 -0600

    fixed my hacky window.location.reload, and changed it to rerender without having to force refresh the page

--- src/EditModal.js
+++ src/EditModal.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import { Button, Input, Modal } from 'semantic-ui-react'
+import { Button, Input, Modal, Icon } from 'semantic-ui-react'
 
 export default class extends Component {
+    state = {
+        modalOpen: false
+    }
+
     editTheItem = async (id) => {
         const editData = {
             method: 'PUT',
@@ -12,26 +16,39 @@ export default class extends Component {
             body: JSON.stringify(this.state)
         }
         await fetch("http://localhost:3001/list", editData)
-        console.log(this.state)
-        window.location.reload()
+        this.props.reRender()
+    }
+
+    handleOpen = () => this.setState({ modalOpen: true })
+
+    handleClose = () => {
+        this.editTheItem()
+        this.setState({ modalOpen: false })
     }
 
     render() {
         console.log(this.props.data)
         return (
-            <Modal size='mini' trigger={<Button color='blue' size='mini' circular icon='edit' />} closeIcon>
-                <Modal.Header>Edit That Item Boi</Modal.Header>
-                <Modal.Content >
+            <Modal
+                trigger={<Button onClick={this.handleOpen} color='blue' size='mini' icon='edit' circular inverted/>}
+                open={this.state.modalOpen}
+                onClose={this.handleClose}
+                size='mini'>
+                <Modal.Content>
                     <Input focus defaultValue={this.props.data.createToDo} onChange={(e) => {
                         this.setState({
                             createToDo: e.target.value,
                             _id: this.props.data._id
                         })
-                    }} /> <Button icon='thumbs up' onClick={() => {
-                        this.editTheItem()
                     }} />
                 </Modal.Content>
-            </Modal >
+                <Modal.Actions>
+                    <Button color='green' onClick={this.handleClose} inverted>
+                        <Icon name='checkmark' /> Change That Item
+          </Button>
+                </Modal.Actions>
+            </Modal>
+
         )
     }
 }
